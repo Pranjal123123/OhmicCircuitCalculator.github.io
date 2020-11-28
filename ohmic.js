a6359e05ef3abecad86f1fa74aee331855837e08
phantomcoder11 <dhritirajborah2020@gmai.com>
2020-11-29 03:20:26 +0530

    bug fixed

--- ohmic.js
+++ ohmic.js
@@ -4,9 +4,9 @@ function myFunction(){
     var R3 = document.getElementById("R3").value;
     var R4 = document.getElementById("R4").value;
     var V = document.getElementById("V").value;
-    var I = (V* (R1 + R2))/(R1*R2 + R1*R3 + R2*R3) ;
     var I2 = (V*R3 )/(R1*R2 + R1*R3 + R2*R3);
     var I3 = (V*R2 )/(R1*R2 + R1*R3 + R2*R3);
+    var I = I2 + I3;
     var text = I +" "+I2 +" "+ I3;
     document.getElementById("demo").innerHTML = text;
 }
